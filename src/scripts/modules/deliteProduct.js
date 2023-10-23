86d3b5a12204a1e175540f2dc9438fd7e5786e70
Anna Hrulckova <annahrulkova@yandex.ru>
2023-10-23 15:43:37 +0400

    итоговое

--- src/scripts/modules/deliteProduct.js
+++ src/scripts/modules/deliteProduct.js
@@ -4,21 +4,21 @@ import { renderingDeliverydProducts } from './renderDeliveryProducts.js'
 import { renderTotalSum } from './renderTotalSum.js'
 import { ordersData } from '../../orderData.js'
 import { increasingProduct } from './increasingProduct.js'
- 
+
 export const deliteProduct = () => {
   const deliteBtn = document.querySelectorAll('.delite')
-  deliteBtn.forEach((btn) => {  
+  deliteBtn.forEach((btn) => {
     btn.addEventListener('click', (event) => {
-      const productIndex = parseInt(event.target.dataset.productIndex);
-      ordersData.splice(productIndex, 1);
+      const productIndex = parseInt(event.target.dataset.productIndex)
+      ordersData.splice(productIndex, 1)
 
-      console.log(ordersData);
-      renderProducts();
-      renderTotalSum();
-      renderingDisabledProducts();
-      renderingDeliverydProducts();
-      deliteProduct();
-      increasingProduct();
+      console.log(ordersData)
+      renderProducts()
+      renderTotalSum()
+      renderingDisabledProducts()
+      renderingDeliverydProducts()
+      deliteProduct()
+      increasingProduct()
     })
   })
 }
